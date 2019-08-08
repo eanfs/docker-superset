d5ca29bde9837fabf624ce809e8b3c1a8d90e376
richard.li <lirichen32@gmail.com>
2019-08-08 12:00:39 +0800

    Update for charset=utf8

--- examples/mysql/superset_config.py
+++ examples/mysql/superset_config.py
@@ -9,6 +9,6 @@ CACHE_CONFIG = {
     'CACHE_REDIS_PORT': 6379,
     'CACHE_REDIS_DB': 1,
     'CACHE_REDIS_URL': 'redis://redis:6379/1'}
-SQLALCHEMY_DATABASE_URI = 'mysql://superset:superset@mysql:3306/superset?useUnicode=true&characterEncoding=utf8&useSSL=false'
+SQLALCHEMY_DATABASE_URI = 'mysql://superset:superset@mysql:3306/superset?charset=utf8'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SECRET_KEY = 'thisISaSECRET_1234'
